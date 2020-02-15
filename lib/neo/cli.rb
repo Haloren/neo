c59c074138f496f6c5a3fd7dff6b10dc677a95fb
Kyle McKenna <mckennakyler@gmail.com>
2020-02-15 04:42:30 +0000

    start refractoring

--- lib/neo/cli.rb
+++ lib/neo/cli.rb
@@ -1,6 +1,6 @@
 class CLI 
   def start  
-    puts "Near Earth Objects(NEOs):"
+    puts "Near Earth Objects (NEOs):"
     puts " Asteroids whose orbit is very close to intersect Earth's orbit."
       Scraper.new.scrape_neos
       menu_options
@@ -12,28 +12,24 @@ class CLI
     puts ""
     puts "What would you like to know?"
     puts "1. Get to know some NEOs" 
-    puts "2. Close passing NEOs"
-    puts "3. 99942 Apophis" #make this an option after a user exits and add credits 
-    puts "4. Exit"
-    puts ""
+    puts "2. Credits" 
+    puts "3. Exit"
+    puts "Please enter a number or type 'Exit': "
   end 
   
   def start_menu
     input = nil 
     while input != "EXIT" 
-      puts "Please enter a number or type 'Exit': " 
       input = gets.strip.upcase 
       case input 
       when "1"
         neos_list
       when "2"
-        close_pass
+        credits  
       when "3"
-        apophis 
-      when "4"
         terminate 
       else 
-        puts "Invaild option. Returning to Main Menu"
+        puts "Returning to Main Menu"
         menu_options
       end
     end   
@@ -41,6 +37,8 @@ class CLI
   
   def terminate
     puts "" 
+    puts "Type 'Exit' to close"
+    puts ""
     puts "Another NEO has passed safely by Earth." 
     puts "See you on the next orbit."
   end   
@@ -49,7 +47,9 @@ class CLI
     puts nil
     Neo.all.each.with_index(1){|n, index| puts "#{index}. #{n.name}" }
  
-    puts "Select a NEO to see how close it will get to Earth and how soon."
+    puts ""
+    puts "To see how close and when the NEO will arrive," #asteroids are listed in closest to furthest
+    puts "Enter a number: "
     
     input = gets.strip.upcase
     index = input.to_i - 1
@@ -68,7 +68,7 @@ class CLI
     puts neo.info
     
     puts "Type 'Yes' to see another NEO"
-    puts "or press any key to return to start."
+    puts "or press any key to return to Main Menu."
     
     input = gets.strip.upcase 
     if input == "YES"
@@ -78,27 +78,17 @@ class CLI
     end   
   end   
     
-  def apophis 
+  def credits 
     puts "" 
-    puts "99942 Apophis was discovered June 19, 2004," 
-    puts "by Roy A. Tucker, David J. Tholen, and Fabrizio Bernardi."
-    puts "With a diameter of 450 meter, taller than the Effile Tower, it will be "
-    puts "visible with the naked eye, on it's closest approach (April 13, 2029)"
-    puts "This approach puts Apophis around 31,000 kilometres from Earth." 
-    puts "That's 10 times closer than the moon!" 
-    puts "So until 2029, Apophis the Great Serpent, we'll be seeing you."
+    puts "NEO Data provided by:"
+    puts "https://theskylive.com/near-earth-objects"
+    puts ""
+    puts "Any key to return to main menu or type 'Exit' to close"
   end   
-  
-  def close_pass   
-    puts "NEOs within half the distance to the moon: "
-    
-  end  
-  
   
 end
 
 
-    
 class Scraper
   def scrape_neos 
     doc = Nokogiri::HTML(open("https://theskylive.com/near-earth-objects"))
@@ -115,7 +105,6 @@ class Scraper
   end 
   
 end 
-    
     
     
 class Neo 
@@ -141,10 +130,10 @@ class Neo
   def info 
     <<~INFO 
     
-    Astroid Name:             #{name}
+    Astroid Name:       #{name}
     
-    Approach Date:        #{date}
-    Nearest Approach :   #{distance} (Lunar Distance)
+    Approach Date:      #{date}
+    Nearest Approach :  #{distance} (Lunar Distance)
     
     INFO
   end 
